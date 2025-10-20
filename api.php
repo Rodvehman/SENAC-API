a37cec98bab260caa705547d1218bf081bf1f7ea
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-10-20 15:28:15 -0300

    Update api.php

--- api.php
+++ api.php
@@ -11,18 +11,22 @@
     // Leitura de arquivo JSON e armazenando em Array Variável.
     $pacocas = json_decode(file_get_contents("pacoca.json"), true);
 
-    if (false){
-        $pacocas['paçocas']['Paçoca de Mel']['nome'] = "Paçoca de Mel";
-        $pacocas['paçocas']['Paçoca de Mel']['tipo'] = "Doce";
-        $pacocas['paçocas']['Paçoca de Mel']['origem'] = "Irlanda";
-        $pacocas['paçocas']['Paçoca de Mel']['nutrientes'] = "Nenhum";
+    function cadastrar_pacoca($nome, $tipo, $origem, $nutrientes){
+            $pacocas['paçocas'][$nome]['nome'] = $nome;
+            $pacocas['paçocas'][$nome]['tipo'] = $tipo;
+            $pacocas['paçocas'][$nome]['origem'] = $origem;
+            $pacocas['paçocas'][$nome]['nutrientes'] = $nutrientes;
+        
+        if (false){
+            salvar_dados($pacocas);
+        }
     }
 
     function salvar_dados($variavel){
     // Salva dados no arquivo
     file_put_contents('pacoca.json', json_encode($pacocas, JSON_PRETTY_PRINT));
     }
-    
+
     // echo $pacocas['paçocas']['Paçoca de Mel']['nome'];
     // echo $pacocas['paçocas']['Paçoca de Côco']['nome'];
     
