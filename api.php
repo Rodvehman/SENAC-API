4b999e55d04ca44e90efb9f97d2055080b1074f4
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-10-20 16:42:49 -0300

    Update

--- api.php
+++ api.php
@@ -10,6 +10,23 @@
     // Leitura de arquivo JSON e armazenando em Array Variável.
     $pacocas = json_decode(file_get_contents("pacoca.json"), true);
 
+    // Variável para guardar na posição enviada pelo link
+    $pacoca_especifica = $_GET['pacoca'];
+    
+    $tipo = $_GET['tipo'];
+
+    switch($pacoca_especifica){
+
+        case "coco":
+            $pacoca_coco = $pacocas['paçocas']['Paçoca de Coco'];
+            echo json_encode($pacoca_coco);
+
+            break;
+        default:
+            echo json_encode($pacocas); 
+            break;
+    }
+
     function cadastrar_pacoca($nome, $tipo, $origem, $nutrientes){
             $pacocas['paçocas'][$nome]['nome'] = $nome;
             $pacocas['paçocas'][$nome]['tipo'] = $tipo;
@@ -31,5 +48,5 @@
     
     // Saída da API
     
-    echo json_encode($alunos); 
+    // echo json_encode($pacocas); 
 ?>
--- cliente.php
+++ cliente.php
@@ -1,9 +1,9 @@
 <?php
 // Consumo de API (Serviço WEB)
 
-    // Requisição através de um URL (protocolo HTTP)
+    // Requisição GET através de um URL (protocolo HTTP)
 
-    $url = "http://localhost/SENAC-API/api.php";
+    $url = "http://localhost/SENAC-API/api.php?pacoca=coco&tipo==doce";
 
     // Guardar um valor de resposta da API
     $resposta = file_get_contents($url);
