b5ae52d49af6d96a49751e843955300e036ac522
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-10-30 17:21:38 -0300

    Api Secreta

--- exercicios/api_secreta/api_secreta.php
+++ exercicios/api_secreta/api_secreta.php
@@ -7,17 +7,38 @@
     switch($metodo){
         case 'GET':
             echo "Acesso ao GET";
+            verificarMetodo();
             break;
         case 'POST':
+            verificarMetodo();
             break;
         case 'PUT':
+            verificarMetodo();
             break;
         case 'DELETE':
+            verificarMetodo();
             break;
         default:
+            verificarMetodo();
             echo "Opção Incorreta";
             break;
     }
 
-    
+    function verificarMetodo(){
+
+        $pcao_metodo = json_decode(file_get_contents('php://input'), true);
+        $texto_padrao = "Método utilizado: ";
+        $tipo_metodo = ['GET', 'POST', 'PUT', 'DELETE'];
+        $opcao_metodo = $metodo;
+
+        if ($opcao_metodo['rest'] === 'GET'){
+            echo json_encode($texto_padrao.$tipo_metodo[0]);
+        } else if ($opcao_metodo['rest'] === 'POST'){
+            echo json_encode($texto_padrao.$tipo_metodo[1]);
+        } else if($opcao_metodo['rest'] === 'PUT'){
+            echo json_encode($texto_padrao.$tipo_metodo[2]);
+        }else if($opcao_metodo['rest'] === 'DELETE'){
+            echo json_encode($texto_padrao.$tipo_metodo[3]);
+        }
+    }
 ?>
--- exercicios/api_secreta/cliente_api_secreta.php
+++ exercicios/api_secreta/cliente_api_secreta.php
@@ -1,3 +1,21 @@
 <?php
-    
+    $url = "http://localhost/SENAC-API/exercicios/api_secreta/api_secreta.php";
+
+    $opcao = [
+        'rest' => 'GET',
+    ];
+
+    $estrutura_http = [
+        'http' => [
+            'method' => 'POST',
+            'header' => 'Content-Type: application/json\r\n',
+            'content' => json_encode($opcao)
+        ]
+    ];
+
+    $contexto = stream_context_create($estrutura_http);
+
+    $resposta = file_get_contents($url, false, $contexto);
+
+    echo $resposta;
 ?>
