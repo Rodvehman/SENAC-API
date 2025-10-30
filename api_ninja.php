7bbbb1c4e2242f302546abc03f6f8dd8999125b0
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-10-30 14:12:13 -0300

    Update api_ninja.php

--- api_ninja.php
+++ api_ninja.php
@@ -3,7 +3,18 @@
     header("Access-Control-Allow-Origin: *");
 
     // Definindo o método de requisição da API
-    $metodo = $_SERVER['method'];
+    $metodo = $_SERVER['REQUEST_METHOD'];
 
-    
+      switch ($metodo){
+        case 'GET':
+            break;
+        case 'POST':
+            break;
+        case 'PUT';
+            break;
+        case 'DELETE':
+            break;
+        default:
+            break;
+      }
 ?>
